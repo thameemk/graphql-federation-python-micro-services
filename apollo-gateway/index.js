38549ddede98f701dd60654a0751a98b0fa67fdb
thameemk612 <thameemk612@gmail.com>
2022-10-14 00:01:09 +0530

    apollo gateway - server

--- apollo-gateway/index.js
+++ apollo-gateway/index.js
@@ -1,11 +1,23 @@
 import {ApolloGateway} from "@apollo/gateway";
+import {startStandaloneServer} from "@apollo/server/standalone";
+import {ApolloServer} from "apollo-server";
 
 const gateway = new ApolloGateway({
     serviceList: [
         {name: 'App 2', url: 'http://127.0.0.1:8901/graphql'},
         {name: 'App 1', url: 'http://127.0.0.1:8900/graphql'}
-    ],
-    introspectionHeaders: {
-        Authorization: 'Bearer abc123'
-    }
+    ]
 });
+
+
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs, resolvers });
+  const { url } = await startStandaloneServer(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+    listen: { port: 4000 },
+  });
+
+  console.log(`🚀  Server ready at ${url}`);
+}
+
+startApolloServer()
